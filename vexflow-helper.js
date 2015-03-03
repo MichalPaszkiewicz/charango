be31e5a5df9a5a314f4ce2cc583006c49f52fc88
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2015-03-03 13:23:31 +0000

    Update vexflow-helper.js

--- vexflow-helper.js
+++ vexflow-helper.js
@@ -2,6 +2,6 @@ var createVex = function(selector){
 	var canvas = document.getElementById(selector);
 	var renderer = new Vex.Flow.Renderer(canvas, Vex.Flow.Renderer.Backends.CANVAS);
 	var ctx = renderer.getContext();
-	var stave = new Vex.Flow.Stave(10, 0, 100);
+	var stave = new Vex.Flow.Stave(10, 0, 200);
 	stave.addClef("treble").setContext(ctx).draw();
 }
