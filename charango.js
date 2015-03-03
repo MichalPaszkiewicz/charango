fb6aa122f32ead0012b6bc5b5ffaf9ce913f3e44
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2015-03-03 13:17:07 +0000

    Update charango.js

--- charango.js
+++ charango.js
@@ -2,4 +2,8 @@ var charangoModule = angular.module('app', []).
 	controller("pageControl", function pageControl($scope){
 		$scope.view = "home";
 	
+		$scope.go = function(view){
+			$scope.view = view;
+			$scope.burger = false;
+		}
 	});
