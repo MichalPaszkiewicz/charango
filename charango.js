98bdc1b436593203c447369415eae10eba4706a8
unknown <michalpaszkiewicz@hotmail.com>
2015-03-03 14:54:41 +0000

    fixing url writing

--- charango.js
+++ charango.js
@@ -1,9 +1,35 @@
 var charangoModule = angular.module('app', []).
 	controller("pageControl", function pageControl($scope){
-		$scope.view = "home";
+
+		$scope.link = location.href;
 	
 		$scope.go = function(view){
 			$scope.view = view;
 			$scope.burger = false;
+			var newSearch = location.host + location.pathname + "?" + 'view=' + view;
+			window.history.pushState(null, null, newSearch);
+			$scope.link = location.href;
 		}
+		
+		$scope.getSearchObjects = function(){
+			var search = location.search;
+			if( search  == null || search == ""){
+				return [{view:"home"}];
+			}
+			
+			var searches = search.substring(1).split("&");
+			
+			var searchObjects = [];
+			
+			for(var i in searches){
+				var splits = searches[i].split("=");
+				var key = splits[0];
+				var value = splits[1];
+				searchObjects[key] = value;
+			}	
+			
+			return searchObjects;
+		}
+		
+		$scope.view = $scope.getSearchObjects().view;
 	});
