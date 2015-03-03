8e2dea66473d7ddc1e97f73892ffac005736001b
unknown <michalpaszkiewicz@hotmail.com>
2015-03-03 23:49:34 +0000

    Display chords

--- charango.js
+++ charango.js
@@ -1,5 +1,5 @@
 var charangoModule = angular.module('app', []).
-	controller("pageControl", function pageControl($scope){
+	controller("pageControl", function pageControl($scope, $timeout){
 
 		$scope.link = location.href;
 	
@@ -9,6 +9,10 @@ var charangoModule = angular.module('app', []).
 			var newSearch = "?" + 'view=' + view;
 			window.history.pushState(null, null, newSearch);
 			$scope.link = location.href;
+			
+			if(view=="chords"){
+				$scope.drawAllChords();
+			}
 		}
 		
 		$scope.getSearchObjects = function(){
@@ -94,5 +98,46 @@ var charangoModule = angular.module('app', []).
 			linkString += "&label=" + $scope.customChord.label;
 			
 			return linkString;
+		}
+		
+		$scope.chords = [
+			{name:"A", notes:[2,1,0,0,0]},
+			{name:"Am", notes:[2,0,0,0,0]},
+			{name:"A#/Bb", notes:[3,2,1,1,1]},
+			{name:"A#m/Bbm", notes:[3,1,1,1,1]},
+			{name:"B", notes:[4,3,2,2,2]},
+			{name:"Bm", notes:[4,2,2,2,2]},
+			{name:"C", notes:[0,0,0,3,0]},
+			{name:"Cm", notes:[0,3,3,3,3]},
+			{name:"C#/Db", notes:[1,1,1,4,1]},
+			{name:"C#m/Dbm", notes:[1,4,4,4,4]},
+			{name:"D", notes:[2,2,2,5,2]},
+			{name:"Dm", notes:[2,5,5,5,5]},
+			{name:"D#/Eb", notes:[3,3,3,6,3]},
+			{name:"D#m/Ebm", notes:[3,6,6,6,6]},
+			{name:"E", notes:[1,4,0,2,0]},
+			{name:"Em", notes:[0,4,3,2,0]},
+			{name:"F", notes:[2,0,1,0,1]},
+			{name:"F#/Gb", notes:[3,1,2,1,2]},
+			{name:"F#m/Gbm", notes:[2,1,2,0,2]},
+			{name:"G", notes:[0,2,3,2,3]},
+			{name:"Gm", notes:[0,2,3,1,3]},
+			{name:"G#/Ab", notes:[1,3,4,3,4]},
+			{name:"G#m/Abm", notes:[1,3,4,2,4]}
+		];
+		
+		$scope.drawAllChords = function(){
+			for(var i in $scope.chords){
+				var notes = [];
+				for(var j in $scope.chords[i].notes){
+					notes.push({value: $scope.chords[i].notes[j]});
+				}
+				
+				$scope.drawChord($scope.chords[i].name, notes)
+			}
+		}
+		
+		if($scope.view=="chords"){
+			$timeout(function(){$scope.go("chords")}, 100);
 		}
 	});
