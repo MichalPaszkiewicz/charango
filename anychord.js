709967625f19ad7dc7a904497d93bf3aaed2e98d
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2015-03-03 10:30:43 +0000

    Update anychord.js

--- anychord.js
+++ anychord.js
@@ -2,3 +2,13 @@ var Chord = function(tuning){
 	this.tuning = [];
 	
 }
+
+var GuitarChord = function(){
+	
+}
+GuitarChord.prototype = new Chord(["e","a","d","g","b","e"]);
+
+var CharangoChord = function(){
+	
+}
+CharangoChord.prototype = new Chord(["g","c","e","a","e"]);
